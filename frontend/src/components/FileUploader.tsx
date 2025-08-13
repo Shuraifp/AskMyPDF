cf401899068c3c4ddbe82134bfdaccd8e9497681
“Shuraifp” <msharraf258@gmail.com>
2025-08-13 09:06:40 +0530

    feat:(frontend)-> implemented auth with clerk. set up ui for upload pdf section

---
+++ frontend/src/components/FileUploader.tsx
@@ -0,0 +1,45 @@
+"use client";
+import * as React from "react";
+
+function FileUploader() {
+  const [file, setFile] = React.useState<File | null>(null);
+  const [error, setError] = React.useState<string | null>(null);
+  const inputRef = React.useRef<HTMLInputElement | null>(null);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = event.target.files?.[0];
+    if (selectedFile) {
+      if (selectedFile.size > 10 * 1024 * 1024) {
+        setError("File size exceeds 10 MB");
+        setFile(null);
+      } else {
+        setError(null);
+        setFile(selectedFile);
+      }
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-2 w-full">
+      <div
+        className="p-6 bg-[#047070] rounded-xl cursor-pointer"
+        onClick={() => {
+          inputRef.current?.click();
+        }}
+      >
+        <input
+          ref={inputRef}
+          type="file"
+          className="hidden"
+          accept="application/pdf"
+          onChange={handleFileChange}
+        />
+        <span>Upload a PDF file</span>
+      </div>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      {file && <p>Selected file: {file.name}</p>}
+    </div>
+  );
+}
+
+export default FileUploader;
